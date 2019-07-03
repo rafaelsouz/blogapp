afd782e2167b1f4d8cebd244177d0ff5d1c3c5e4
Rafael <raffaelsouza908@gmail.com>
2019-07-02 22:14:39 -0300

    make it better

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://rafael:teste123@blogapp-prod-u4uat.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb+srv://rafael:Esp@d@44@blogapp-prod-u4uat.mongodb.net/test?retryWrites=true&w=majority'}
 }else{
     module.exports= {mongoURI: 'mongodb://localhost:27017/blogapp'}
 }
